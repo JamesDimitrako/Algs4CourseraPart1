0f3ac29e1e38e84a9b4ef307be33fffed47deb38
JamesDimitrako <15272980+JamesDimitrako@users.noreply.github.com>
2020-02-05 02:14:22 +0200

    Add week 2 classes

--- src/WeightedQuickUnion.java
+++ src/WeightedQuickUnion.java
@@ -1,9 +1,10 @@
-package uom.cs;
+import uom.cs.QuickUnionUF;
 
-public class QuickUnionUF {
+public class WeightedQuickUnion {
     private int[] id;
+    private int[] sz;
 
-    public QuickUnionUF(int N) {
+    public WeightedQuickUnion(int N) {
         id = new int[N];
         for (int i = 0; i < N; i++) id[i] = i;
     }
